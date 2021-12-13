5b8b25ed6ef95d9843766aaffaec7d89aadb3f8e
JesusVasari <jlopez 2rafaelalberti.es>
2021-12-13 21:41:39 +0100

    3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,56 @@
-open class armaFuego(municion: Int) {
-    private var nombre: String=""
-    private var municion: Int = 0
-    private var tipoMunicion: String=""
-    private var danyo: Int = 0
-    private var radio = String
+open class armaFuego(
+    private var nombre: String = "",
+    protected var municion: Int = 0,
+    protected var restar: Int = 1,
+    private var tipoMunicion: String = "",
+    private var daño: Int = 0,
+    private var radio: String = "pequeño"
+) {
+
 
     override fun toString(): String {
-        return  "$nombre, $municion, $tipoMunicion, $danyo, $radio"
+        return "$nombre, $municion, $tipoMunicion, $daño, $radio"
     }
 
-    open fun recarga() = municion
-    open fun dispara() = municion
+    open fun recarga(): Int = municion + 6
+    open fun dispara(): Int = restar
 }
 
-class Pistola(municion:Int) : armaFuego(municion) {
+class Pistola(nombre: String, municion: Int,restar:Int, tipoMunicion: String, daño: Int,radio: String) : armaFuego(nombre, municion,restar, tipoMunicion, daño,radio) {
 
-    final override fun dispara(): Int = -1
-
+    override fun dispara():Int {
+        municion -= restar * 1
+        return municion
+    }
 
 }
-class Rifle(municion: Int) : armaFuego(municion) {
 
-    final override fun dispara(): Int = -2
-}
-class bazooka(municion: Int) : armaFuego(municion) {
+class Rifle(nombre: String, municion: Int,restar:Int, tipoMunicion: String, daño: Int,radio: String) : armaFuego(nombre, municion,restar, tipoMunicion, daño,radio) {
 
-    final override fun dispara(): Int = -3
+    override fun dispara(): Int {
+        municion -= restar * 2
+        return municion
+    }
 }
+
+class Bazooka(nombre: String, municion: Int,restar:Int, tipoMunicion: String, daño: Int,radio: String) :armaFuego(nombre, municion,restar, tipoMunicion, daño,radio) {
+
+    override fun dispara():Int {
+        municion -= restar * 3
+        return municion
+    }
+}
+
 fun main() {
-
-    var p1 : Pistola = Pistola(20)
+  // funciona la instancia menos restar
+    var p1: Pistola = Pistola("magnun",18,1,"44mm",50,"grande")
     p1.dispara()
-
     println(p1)
 
-    var p2 :Rifle = Rifle(30)
+
+    var p2 :Rifle = Rifle("remington",15,0,"shoot",18,"grande")
+    var p3: Bazooka = Bazooka("oefmn",11,0,"granada",100,"grande")
+
+    var mapa :Map<K, out V>
+    println()
 }
